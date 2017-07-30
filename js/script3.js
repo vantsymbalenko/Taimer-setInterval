c94661226a120c42bb5d16dba5ac2947e8b22279
vantsymbalenko <IvanZymbalenko1@gmail.com>
2017-07-30 16:58:17 +0300

    Fixed formatting

--- js/script3.js
+++ js/script3.js
@@ -1,68 +1,61 @@
-function Taimer(){
-  this.minute=document.body.querySelector(".minute");
-  this.seconds=document.body.querySelector(".seconds");
-  this.mseconds=document.body.querySelector(".mseconds");
-  var accept=1;
+function Taimer() {
+  var accept = 1;
+  var ul = document.body.querySelector("ul");
 
-  
-  this.start = function(){
-    
-    if(accept==0){
+  this.minutes = document.body.querySelector(".minutes");
+  this.seconds = document.body.querySelector(".seconds");
+  this.mseconds = document.body.querySelector(".mseconds");
+
+  this.start = function() {
+    if (accept == 0) {
       return;
     }
-    accept=0;
-    this.timmerIdMinute=setInterval(this.grow,60000,this.minute,1);
-    this.timmerIdSecond=setInterval(this.grow,1000,this.seconds,2);
-    this.timmerIdMsecond=setInterval(this.grow,1,this.mseconds,3);
+    accept = 0;
+    this.timmerIdMinutes = setInterval(this.grow, 60000, this.minutes, 1);
+    this.timmerIdSeconds = setInterval(this.grow, 1000, this.seconds, 2);
+    this.timmerIdMseconds = setInterval(this.grow, 1, this.mseconds, 3);
+  }
 
-  }
-  
-  this.grow = function(element,id){
-    if(((id==1 || id==2) && +element.innerHTML>58) || (id==3 && +element.innerHTML>98)){
-      element.innerHTML=0;
-    }
-    else{
-      element.innerHTML=+element.innerHTML+1;      
+  this.grow = function(element, id) {
+    if (((id == 1 || id == 2) && +element.innerHTML > 58) || (id == 3 && +element.innerHTML > 98)) {
+      element.innerHTML = 0;
+    } else {
+      element.innerHTML = +element.innerHTML + 1;
     }
   }
-  
-  this.stop = function(){
-    accept=1;
-    
-    clearInterval(this.timmerIdMinute);
-    clearInterval(this.timmerIdSecond);
-    clearInterval(this.timmerIdMsecond);   
+
+  this.stop = function() {
+    accept = 1;
+    clearInterval(this.timmerIdMinutes);
+    clearInterval(this.timmerIdSeconds);
+    clearInterval(this.timmerIdMseconds);
   }
-  
-  this.lap=function(){
-    if(accept==1){
+
+  this.lap = function() {
+    var li = document.createElement("li");
+    if (accept == 1) {
       return;
     }
-    
-    var li=document.createElement("li");
     li.classList.add("listLap");
-    var ul=document.body.querySelector("ul");
-    li.innerHTML=this.minute.innerHTML + ":" + this.seconds.innerHTML + ":" + this.mseconds.innerHTML;
+    li.innerHTML = this.minutes.innerHTML + ":" + this.seconds.innerHTML + ":" + this.mseconds.innerHTML;
     ul.appendChild(li);
   }
-  
-  this.reset=function(){
-    accept=1;
-    
-    this.minute.innerHTML=0;
-    this.seconds.innerHTML=0;
-    this.mseconds.innerHTML=0;
 
-    clearInterval(this.timmerIdMinute);
-    clearInterval(this.timmerIdSecond);
-    clearInterval(this.timmerIdMsecond);   
-    
-    var ul=document.body.querySelector("ul");
-    while(ul.firstChild){
+  this.reset = function() {
+    accept = 1;
+    this.minutes.innerHTML = 0;
+    this.seconds.innerHTML = 0;
+    this.mseconds.innerHTML = 0;
+
+    clearInterval(this.timmerIdMinutes);
+    clearInterval(this.timmerIdSeconds);
+    clearInterval(this.timmerIdMseconds);
+
+    while (ul.firstChild) {
       ul.removeChild(ul.firstChild);
     }
   }
-  
+
   this.start = this.start.bind(this);
   this.grow = this.grow.bind(this);
   this.stop = this.stop.bind(this);
@@ -71,9 +64,7 @@ function Taimer(){
 }
 
 var taimer = new Taimer();
-
-
-document.body.querySelector(".start").addEventListener("click",taimer.start);
-document.body.querySelector(".stop").addEventListener("click",taimer.stop);
-document.body.querySelector(".lap").addEventListener("click",taimer.lap);
-document.body.querySelector(".reset").addEventListener("click",taimer.reset);
+document.body.querySelector(".start").addEventListener("click", taimer.start);
+document.body.querySelector(".stop").addEventListener("click", taimer.stop);
+document.body.querySelector(".lap").addEventListener("click", taimer.lap);
+document.body.querySelector(".reset").addEventListener("click", taimer.reset);
